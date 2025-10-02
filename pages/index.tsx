b9cb64d98b0c53849674e7ed93012be41ae45bc0
TheCodePioneer <150111051+TheCodePioneer@users.noreply.github.com>
2025-10-02 15:20:04 +0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Next.js + Tailwind + TypeScript!
+      </h1>
+    </div>
+  )
+}
